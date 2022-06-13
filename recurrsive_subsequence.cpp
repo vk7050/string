77c5d147130978551658f5c9d928705af6911c58
vk7050 <84660046+vk7050@users.noreply.github.com>
2022-06-13 17:43:48 +0530

    Update recurrsive_subsequence.cpp

--- recurrsive_subsequence.cpp
+++ recurrsive_subsequence.cpp
@@ -1,8 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+bool Subsequence(string &str,string &str1,int n,int m)
+{
+  if(m==0)
+    return true;
+  if(n==0)
+    return false;
+  if(str[n-1]==str1[m-1])
+    return Subsequence(str,str1,n-1,m-1);
+  return Subsequence(str,str1,n-1,m);
+}
+     
+
 int main()
 {
   string str,str1;
   cin>>str;
   cin>>str1;
-  Subsequence(str,str1,str.lenght(),str1.length())
+  bool a=Subsequence(str,str1,str.lenght(),str1.length());
+  cout<<a<<endl;
+  return 0;
+}
